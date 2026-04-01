68ffad9b3ee0a7a35270a46e7819bda727a413b3
Ja'Quis <jaquismay2004@gmail.com>
2026-04-01 11:35:52 -0400

    Deleted unused code in the server

--- webServer.js
+++ webServer.js
@@ -1,12 +1,9 @@
 import express from 'express';
+//Using vite for jsx translation
 import ViteExpress from 'vite-express'
+
 const app = express()
 const hostPort = 3000
-
-app.get('/api/status', (req, res) => {
-  console.log("API hit!");
-  res.send("Triple-Ts API is online.");
-});
 
 ViteExpress.listen(app, hostPort, () =>{
   console.log(`Server Listening at http://localhost:${hostPort}`)
